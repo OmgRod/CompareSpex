608a238d06ffc50f46e5a6926bb340b9fb41bafa
0mgRod <rodrigofonseca20@outlook.com>
2024-03-22 19:53:26 +0000

    Added README.md and License

---
+++ README.md
@@ -0,0 +1,12 @@
+# CompareSpex
+
+## Installation
+
+1. Download [Node.js](https://nodejs.org/) if you haven't already.
+2. Open cmd or terminal on the downloaded extracted folder of the project.
+3. Type in `npm i` to install all the dependencies.
+4. Lastly, type in `node index` to run the project.
+
+## License
+
+This project uses the [MIT License](LICENSE)
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const port = 4000;
@@ -10,6 +11,18 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware to extract IP address from the request
+app.use((req, res, next) => {
+    // Getting the IP address from the request headers
+    const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    
+    // Storing the IP address in the request object for later use
+    req.ipAddress = ipAddress;
+    
+    // Pass control to the next middleware or route handler
+    next();
+  });
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -21,6 +34,14 @@ app.get('/request', (req, res) => {
 
 app.get('/profile', (req, res) => {
     res.render('profile');
+});
+
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+
+app.get('/compare', (req, res) => {
+    res.render('compare');
 });
 
 app.listen(port, () => {
