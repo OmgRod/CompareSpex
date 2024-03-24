d1e52d6c042bd19dadfa5a24f1e6f17a2c3ea095
0mgRod <89850217+0mgRod@users.noreply.github.com>
2024-03-24 12:41:07 +0000

    Added cookie policy

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const path = require('path');
 const fs = require('fs');
 const { promisify } = require('util');
 const readFileAsync = promisify(fs.readFile);
-const rateLimit = require("express-rate-limit");
 require('dotenv').config();
 
 const app = express();
@@ -15,13 +14,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Define rate limiting options
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-  message: "Too many requests from this IP, please try again later"
-});
 
 
 
@@ -40,39 +32,9 @@ if (!db.tableExists('users')) {
 
 
 
-// Middleware to extract IP address from the request
-app.use((req, res, next) => {
-    // Getting the IP address from the request headers
-    const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-    // Storing the IP address in the request object for later use
-    req.ipAddress = ipAddress;
-
-    // Pass control to the next middleware or route handler
-    next();
-});
-
-// Apply the rate limiter middleware to the specified route
-app.use('/profile/:userId', limiter, async (req, res, next) => {
-    try {
-        // Assuming user profile images are stored in the 'static/pfp' directory
-        const imagePath = path.join(__dirname, 'static', 'pfp', `${req.params.userId}.png`);
-
-        // Read the image file asynchronously
-        const image = await readFileAsync(imagePath);
-
-        // Convert the image to base64
-        const base64Image = Buffer.from(image).toString('base64');
-
-        // Pass the base64 data to the EJS template
-        res.locals.profileImageBase64 = base64Image;
-
-        // Continue to the next middleware
-        next();
-    } catch (error) {
-        // If there's an error reading the file or converting to base64, pass it to the error handling middleware
-        next(error);
-    }
+app.get('/profile/:userId', (req, res) => {
+    // Render the profile template with the provided userId
+    res.render('profile', { userId: req.params.userId, base64Image: res.locals.profileImageBase64 });
 });
 
 
@@ -103,6 +65,10 @@ app.get('/settings', (req, res) => {
 
 app.get('/pricing', (req, res) => {
     res.render('pricing');
+});
+
+app.get('/cookie-policy', (req, res) => {
+    res.render('cookiepolicy');
 });
 
 // Error handling middleware
